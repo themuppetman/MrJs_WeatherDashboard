84d977f521c29876b8adb0add24eacc3474f7d25
themuppetman <157922635+themuppetman@users.noreply.github.com>
2024-03-17 17:56:47 -0700

    Attempting BS/Foundation

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+const APIKey = "02dcac2fc37c507c8f72bb6a0a01b040";
+
+let city = "https://api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&appid={API key}"
+
+
+api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
